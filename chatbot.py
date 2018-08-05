5c9f803e6f20da79b19ba540f8f87d2633ae240f
root <root@trainXtrainer.localdomain>
2018-08-05 22:22:13 +0000

    The basic model was developed with some simple questions

---
+++ chatbot.py
@@ -0,0 +1,121 @@
+from pyspark.sql import SQLContext
+from pyspark import SparkContext
+from pyspark.ml.feature import RegexTokenizer, StopWordsRemover,CountVectorizer
+from pyspark.ml.classification import LogisticRegression
+
+sc = SparkContext()
+sqlContext = SQLContext(sc)
+language = "english"
+data = None
+regexTokenizer = None
+stopwordsRemover = None
+countVectors = None
+dataset = None
+
+def init():
+
+    global data,regexTokenizer,stopwordsRemover,countVectors
+    
+    # Read the seed training data into the system
+    
+    data = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load('seeddata.csv')
+    
+    # Contains a list of columns we don't care about
+    
+    drop_list = []
+    
+    # Grab data in all columns that we care about
+    data = data.select([column for column in data.columns if column not in drop_list])
+
+    data.show(5)
+
+    # regular expression tokenizer
+    regexTokenizer = RegexTokenizer(inputCol="question",outputCol="words", pattern="\\W")
+
+    # stop words
+    add_stopwords = None
+
+    # Load some default stopwords
+    if add_stopwords == None:
+        add_stopwords = StopWordsRemover.loadDefaultStopWords(language)
+
+        # Remove certain stop words that provide context for our use case
+        needed_stopwords = ['what','when','where','why']
+        for x in needed_stopwords:
+            add_stopwords.remove(x)
+        print("stop words:\n {}".format(add_stopwords))
+    
+    stopwordsRemover = StopWordsRemover(inputCol="words",outputCol="filtered").setStopWords(add_stopwords)
+
+    # bag of words count
+    countVectors = CountVectorizer(inputCol="filtered",outputCol="features", vocabSize=15, minDF=1)
+
+
+def featureExtraction():
+
+    from pyspark.ml import Pipeline
+    from pyspark.ml.feature import OneHotEncoder, StringIndexer, VectorAssembler
+
+    global dataset
+
+    # Encode a string column of labels ordered by label frequencies
+
+    label_stringIdx = StringIndexer(inputCol = "category", outputCol="label")
+
+    pipeline = Pipeline(stages=[regexTokenizer,stopwordsRemover,countVectors, label_stringIdx])
+
+    # Fit the pipeline to training data
+    pipelineFit = pipeline.fit(data)
+    dataset = pipelineFit.transform(data)
+    dataset.show(10)
+
+
+def train(interations):
+
+    global testData, lrModel
+
+    # Split the data into 70% training data
+    #(trainingData, testData) = dataset.randomSplit([0.7, 0.3], seed=100)
+    
+    # Train with all the data because we have very little data
+    trainingData  = dataset
+    
+    lr = LogisticRegression(maxIter=interations, regParam=0.3, elasticNetParam=0)
+    lrModel = lr.fit(trainingData)
+
+# Predict using the testData if traintest is False.  Otherwise, predict based on the data parameter, which should be a dataframe
+def predict(traintest=False,data=None):
+
+    if traintest == True:
+        inputData = testData
+    else:
+        inputData = data
+
+    predictions = lrModel.transform(inputData)
+
+    predictions.filter(predictions['prediction'] >= 0) \
+            .select("question","answer","probability","label","prediction") \
+            .orderBy("probability",ascending=False) \
+            .show(n=10,truncate=30)
+
+
+def driver():
+    while True:
+        try:
+
+            question = input(":=> ")
+        except ValueError:
+            print(":=> Sorry, I didn't understand that.")
+            continue
+        else:
+            break
+
+if __name__ == "__main__":
+    print("Afrotech Chatbot\n")
+    init()
+    featureExtraction()
+    clientData = dataset.sample(False,0.30)
+    for x in range(100,1000,20):
+        train(x)
+        predict(data=clientData)
+    #driver()
